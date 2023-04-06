4c60bb99d82f3519537d86c3e82482bb4de6ec70
Katarzyna Farian <katarzyna.farian@boldare.com>
2023-04-06 22:21:12 +0200

    Load Backobne script and list down peaple name and age

--- index.js
+++ index.js
@@ -1,58 +1,16 @@
-const http = require('http');
-const fs = require('fs');
+const express = require('express');
 const path = require('path');
+const app = express();
 
-const server = http.createServer(function (req, res) {
-    // Parse the URL
-    const url = new URL(req.url, `http://${req.headers.host}`);
+// Serve static files from the public directory
+app.use(express.static(path.join(__dirname, 'public')));
 
-    // Serve index.html by default
-    let filePath = './public/index.html';
-
-    // Check if a different file was requested
-    if (url.pathname !== '/') {
-        filePath = `./public${url.pathname}`;
-    }
-
-    // Get the file extension
-    const extname = path.extname(filePath);
-
-    // Set the Content-Type header based on the file extension
-    let contentType = 'text/html';
-    switch (extname) {
-        case '.js':
-            contentType = 'text/javascript';
-            break;
-        case '.css':
-            contentType = 'text/css';
-            break;
-    }
-
-    // Read the file and serve it
-    fs.readFile(filePath, function(error, content) {
-        if (error) {
-            if(error.code == 'ENOENT'){
-                // File not found
-                res.writeHead(404);
-                res.end('404 Not Found');
-            }
-            else {
-                // Server error
-                res.writeHead(500);
-                res.end('500 Internal Server Error');
-            }
-        }
-        else {
-            // Success
-            res.writeHead(200, { 'Content-Type': contentType });
-            res.end(content, 'utf-8');
-        }
-    });
-
+// Route for the index page
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-const port = process.env.PORT || 8081;
-server.listen(port, function () {
-    console.log(`Server listening on port ${port}`);
+// Start the server
+app.listen(8081, function() {
+    console.log('Server started on port 8081');
 });
-
